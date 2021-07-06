05ce043147534879602c9a6772fef56754a75d4d
Charlotte Soneson <charlottesoneson@gmail.com>
2021-07-06 17:13:32 +0200

    Some more tests

--- tests/testthat/test-object_check.R
+++ tests/testthat/test-object_check.R
@@ -11,8 +11,21 @@ test_that("downloading droplet data works", {
     expect_setequal(SingleCellExperiment::reducedDimNames(droplet_li),
                     c("PCA", "UMAP", "TSNE"))
     expect_equal(colnames(SingleCellExperiment::reducedDim(droplet_li, "PCA")),
-                 paste0("PC", seq_len(ncol(SingleCellExperiment::reducedDim(droplet_li, "PCA")))))
+                 paste0("PC", seq_len(ncol(
+                     SingleCellExperiment::reducedDim(droplet_li, "PCA")))))
+    expect_equal(colnames(SingleCellExperiment::reducedDim(droplet_li, "TSNE")),
+                 paste0("TSNE", seq_len(ncol(
+                     SingleCellExperiment::reducedDim(droplet_li, "TSNE")))))
+    expect_equal(colnames(SingleCellExperiment::reducedDim(droplet_li, "UMAP")),
+                 paste0("UMAP", seq_len(ncol(
+                     SingleCellExperiment::reducedDim(droplet_li, "UMAP")))))
     expect_equal(unique(as.character(droplet_li$tissue)), "Large_Intestine")
+
+    ## Without reducedDims
+    droplet <- TabulaMurisSenisDroplet(tissues = "Large_Intestine",
+                                       processedCounts = FALSE,
+                                       reducedDims = FALSE)
+    expect_equal(length(SingleCellExperiment::reducedDims(droplet[[1]])), 0)
 })
 
 test_that("downloading facs data works", {
@@ -28,8 +41,21 @@ test_that("downloading facs data works", {
     expect_setequal(SingleCellExperiment::reducedDimNames(facs_ao),
                     c("PCA", "UMAP", "TSNE"))
     expect_equal(colnames(SingleCellExperiment::reducedDim(facs_ao, "PCA")),
-                 paste0("PC", seq_len(ncol(SingleCellExperiment::reducedDim(facs_ao, "PCA")))))
+                 paste0("PC", seq_len(ncol(
+                     SingleCellExperiment::reducedDim(facs_ao, "PCA")))))
+    expect_equal(colnames(SingleCellExperiment::reducedDim(facs_ao, "TSNE")),
+                 paste0("TSNE", seq_len(ncol(
+                     SingleCellExperiment::reducedDim(facs_ao, "TSNE")))))
+    expect_equal(colnames(SingleCellExperiment::reducedDim(facs_ao, "UMAP")),
+                 paste0("UMAP", seq_len(ncol(
+                     SingleCellExperiment::reducedDim(facs_ao, "UMAP")))))
     expect_equal(unique(as.character(facs_ao$tissue)), "Aorta")
+
+    ## Without reducedDims
+    facs <- TabulaMurisSenisFACS(tissues = "Aorta",
+                                 processedCounts = FALSE,
+                                 reducedDims = FALSE)
+    expect_equal(length(SingleCellExperiment::reducedDims(facs[[1]])), 0)
 })
 
 test_that("downloading bulk data works", {
