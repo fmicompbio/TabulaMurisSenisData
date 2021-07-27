a4e9adbd4db8885851f49cac5fe89e8c8547b960
Charlotte Soneson <charlottesoneson@gmail.com>
2021-07-27 11:36:54 +0200

    Add unit tests

--- tests/testthat/test-object_check.R
+++ tests/testthat/test-object_check.R
@@ -28,6 +28,17 @@ test_that("downloading droplet data works", {
                  paste0("UMAP", seq_len(ncol(
                      SingleCellExperiment::reducedDim(droplet_li, "UMAP")))))
     expect_equal(unique(as.character(droplet_li$tissue)), "Large_Intestine")
+    expect_equal(unique(as.character(droplet_li$method)), "droplet")
+
+    ## Test that processed counts correspond to raw counts
+    cts <- as.matrix(t(assay(droplet_li, "counts")))
+    proc_counts <- log2(cts/rowSums(cts) * 1e4 + 1)
+    proc_counts <- t(t(proc_counts)/sqrt(colVars(proc_counts)))
+    proc_counts[is.na(proc_counts)] <- 0
+    proc_counts[proc_counts > 10] <- 10
+    expect_true(
+        max(abs(proc_counts - as.matrix(t(assay(droplet_li, "logcounts"))))) < 1e-5
+    )
 
     ## Without reducedDims and processedCounts
     droplet <- TabulaMurisSenisDroplet(tissues = "Large_Intestine",
@@ -67,6 +78,17 @@ test_that("downloading facs data works", {
                  paste0("UMAP", seq_len(ncol(
                      SingleCellExperiment::reducedDim(facs_ao, "UMAP")))))
     expect_equal(unique(as.character(facs_ao$tissue)), "Aorta")
+    expect_equal(unique(as.character(facs_ao$method)), "facs")
+
+    ## Test that processed counts correspond to raw counts
+    cts <- as.matrix(t(assay(facs_ao, "counts")))
+    proc_counts <- log2(cts/rowSums(cts) * 1e4 + 1)
+    proc_counts <- t(t(proc_counts)/sqrt(colVars(proc_counts)))
+    proc_counts[is.na(proc_counts)] <- 0
+    proc_counts[proc_counts > 10] <- 10
+    expect_true(
+        max(abs(proc_counts - as.matrix(t(assay(facs_ao, "logcounts"))))) < 1e-5
+    )
 
     ## Without reducedDims and processedCounts
     facs <- TabulaMurisSenisFACS(tissues = "Aorta",
